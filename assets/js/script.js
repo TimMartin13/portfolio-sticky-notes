6a5f84772a91b6251fe6065d93526639dda4c8c4
Tim Martin <timmartin13@gmail.com>
2021-02-20 14:27:11 -0600

    Got the portfolio page working

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Change the color of the list items randomly based on an array of colors
+$('section').each(function() {
+    // [Orange, Yellow, Pink, Blue, Yellow, Green]
+    var back = ["#F9A55B","#ffc","#ff7eb9","#7afcff","#ffc", "#98FB98"];
+    var rand = back[Math.floor(Math.random() * back.length)];
+    $(this).css('background-color',rand);
+});
